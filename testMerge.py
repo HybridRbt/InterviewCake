b8e046f2d511738b4f6dc797b12026cba287e89a
Jianfeng Yang <jianfeng.yang0120@gmail.com>
2015-04-20 11:31:03 -0500

    add new files

---
+++ testMerge.py
@@ -0,0 +1,13 @@
+__author__ = 'JeredYang'
+
+import MergeMeetingTiimes as mmt
+
+def testMerge():
+    meeting_list = [(0, 1), (3, 5), (4, 8), (10, 12), (9, 10)]
+    print "Original list: \n" + str(meeting_list)
+    condensed_list = mmt.merge_meeting_time(meeting_list)
+    assert type(condensed_list) == list
+    assert condensed_list == [(0, 1), (3, 8), (9, 12)]
+    print "Condensed list: \n" + str(condensed_list)
+
+testMerge()
