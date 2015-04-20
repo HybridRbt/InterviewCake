6b221a5bec1d3de29fce5be97443e920bdbc1a06
Jianfeng Yang <jianfeng.yang0120@gmail.com>
2015-04-20 11:56:03 -0500

    For loop won't work.
    Changed to while loop.

--- MergeMeetingTiimes.py
+++ MergeMeetingTiimes.py
@@ -1,24 +1,21 @@
 __author__ = 'JeredYang'
 
-list = []
-list.sort()
-
-
 def combine_time(first_time, second_time):
     return first_time[0], second_time[1]
-
 
 def merge_meeting_time(meeting_time_list):
     new_list = meeting_time_list
     new_list.sort(key=lambda x:x[0])
     result = []
-    for index in range(len(new_list) - 1):
+    index = 0
+    while index < len(new_list) - 1:
         if new_list[index][1] < new_list[index + 1][0]:
             result.append(new_list[index])
-            result.append(new_list[index + 1])
+            #result.append(new_list[index + 1])
+            index += 1
         else:
             new_time = combine_time(new_list[index], new_list[index + 1])
             result.append(new_time)
-        index += 2
+            index += 2
 
     return result
