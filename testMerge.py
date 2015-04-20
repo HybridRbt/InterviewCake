1fe7b380aea71353a79d4cf3c6a532e72ea9f44e
Jianfeng Yang <jianfeng.yang0120@gmail.com>
2015-04-20 11:56:55 -0500

    change test case for those "touch" case
    This will fail with the original code.

--- testMerge.py
+++ testMerge.py
@@ -3,7 +3,7 @@ __author__ = 'JeredYang'
 import MergeMeetingTiimes as mmt
 
 def testMerge():
-    meeting_list = [(0, 1), (3, 5), (4, 8), (10, 12), (9, 10)]
+    meeting_list = [(0, 1), (1, 2), (2, 3), (3, 5), (4, 8), (10, 12), (9, 10)]
     print "Original list: \n" + str(meeting_list)
     condensed_list = mmt.merge_meeting_time(meeting_list)
     assert type(condensed_list) == list
